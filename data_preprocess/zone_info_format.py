a9bf4c11b479aa8e9750a6fa2a820221cba20eb7
jinchen1036 <jinchen1036@users.noreply.github.com>
2020-11-10 18:09:35 -0500

    match taxi zone with zipcode and county

--- data_preprocess/zone_info_format.py
+++ data_preprocess/zone_info_format.py
@@ -1,17 +1,22 @@
 import pandas as pd
-from  data_preprocess.zone_distance import get_zone_centroid
+from uszipcode import SearchEngine
+from data_preprocess.zone_distance import get_zone_centroid
 
 taxi_zones = pd.read_csv("data/taxi_zones.csv")
+search = SearchEngine(simple_zipcode=True)
 zones_info = []
 
 for index, row in taxi_zones.iterrows():
     x, y = get_zone_centroid(row['the_geom'])
+    result = search.by_coordinates(y, x)[0]
     zone_info = {
         'location_id': row['LocationID'],
         'zone': row['zone'],
         'borough': row['borough'],
         'centroid_x': x,
-        'centroid_y': y
+        'centroid_y': y,
+        'zipcode': result.zipcode,
+        'county':result.county,
     }
     zones_info.append(zone_info)
 
