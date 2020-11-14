ab18359618ad45109ddde5edae9079b33731b4b8
jma008 <jma008@citymail.cuny.edu>
2020-11-13 23:50:06 -0500

    clean trip data and remove cache

--- data_preprocess/trip_data_format.py
+++ data_preprocess/trip_data_format.py
@@ -1,7 +1,9 @@
 import vaex
 import pandas as pd
+from datetime import timedelta
 from data.config import boroughs, weekday_names
 from dataAccessor import get_taxi_data,get_zone_info
+
 
 year = 2020
 month = 3
@@ -12,15 +14,19 @@ trip_data = get_taxi_data(year = year, month = month)
 # Get only need columns
 trip_data = trip_data[['tpep_pickup_datetime','tpep_dropoff_datetime', 'passenger_count','trip_distance','PULocationID', 'DOLocationID','payment_type','total_amount']]
 
+trip_data = trip_data.iloc[:10000]
+
 # show not null count
 trip_data.info(verbose=True, null_counts=True)
 
 # TO DO: please deal with the row that have nan value
+trip_data.dropna(inplace = True)
 
 # TO DO: should not include the travel distance to be 0 or less
+trip_data = trip_data[trip_data['trip_distance']>0]
 
 # TO DO: should not include the total price to be 0 or less
-
+trip_data = trip_data[trip_data['total_amount']>0]
 # TO DO:  Please see the `Format_Trip_Data_Note.md` for the payment type detail, decide if should only include Credit card and Cash payment or all
 #         After make the decision, please add a payment_type dict in data/config.py
 # - payment_type
@@ -30,19 +36,23 @@ trip_data.info(verbose=True, null_counts=True)
 #     - 4 = Dispute
 #     - 5 = Unknown
 #     - 6 = Voided trip
+trip_data = trip_data[(trip_data['payment_type']==1) | (trip_data['payment_type']==2)]
 
 # TO DO: check for other conditions  (e.g., `tpep_dropoff_datetime` should be greater than `tpep_pickup_datetime`)
-
+different_time = trip_data['tpep_dropoff_datetime'] - trip_data['tpep_pickup_datetime']
+trip_data = trip_data[different_time > timedelta(minutes=1)]
 # can use below line to explore a sample record in the dataset
 row = trip_data.iloc[0]
+def get_trip_duration(trip):
+    return (trip['tpep_dropoff_datetime']  - trip['tpep_pickup_datetime']).total_seconds()/60
 
+trip_data['trip_duration_min'] = trip_data.apply(get_trip_duration, axis=1)
+trip_data['trip_speed_mph'] = trip_data['trip_distance'] /( trip_data['trip_duration_min'] / 60)
 
 
 # Extract meaningful info
 format_trips = []
 for index, row in trip_data.iterrows():
-    trip_duration_min = (row['tpep_dropoff_datetime']  - row['tpep_pickup_datetime']).total_seconds()/60
-
     # TO DO: Add other fields that you think should be include (note: all field should be numeric value in order to store in hdf5 file)
     # Can separate time into years and months if you want
     trip = {
@@ -62,8 +72,8 @@ for index, row in trip_data.iterrows():
         'payment_type': row['payment_type'],
 
         'trip_distance': row['trip_distance'],
-        'trip_duration_min': trip_duration_min,
-        'trip_speed_mph': row['trip_distance']/(trip_duration_min/60)
+        'trip_duration_min': row['trip_duration_min'],
+        'trip_speed_mph': row['trip_speed_mph']
     }
 
     format_trips.append(trip)
