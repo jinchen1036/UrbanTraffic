c985b1bad775dadf0f317e03dd0a7f9f9b419a3d
bsl <bosenli2013@hotmail.com>
2020-11-15 17:56:07 -0500

    concat taxi data

--- data_preprocess/yellow_taxi_count.py
+++ data_preprocess/yellow_taxi_count.py
@@ -10,13 +10,10 @@ yellow_taxi_2020_03 = pd.read_csv('data/yellow_taxi_2020_03_count.csv')
 yellow_taxi_2020_04 = pd.read_csv('data/yellow_taxi_2020_04_count.csv')
 yellow_taxi_2020_05 = pd.read_csv('data/yellow_taxi_2020_05_count.csv')
 
-group_data = pd.merge(yellow_taxi_2019_03, yellow_taxi_2019_04, yellow_taxi_2019_05, yellow_taxi_2020_03, yellow_taxi_2020_04, yellow_taxi_2020_05, how='left', left_on=['time'],
-right_on=['time'])
+yellow_taxi_group_data = pd.concat([yellow_taxi_2019_03,yellow_taxi_2019_04, yellow_taxi_2019_05,
+                        yellow_taxi_2020_03, yellow_taxi_2020_04, yellow_taxi_2020_05])
 
 
 from uszipcode import SearchEngine
-search = SearchEngine(simple_zipcode=True)
 
 
-nan_data = group_data[group_data['median_home_value'].isna()]
-zip_code = list(nan_data['zipcode'].values)
