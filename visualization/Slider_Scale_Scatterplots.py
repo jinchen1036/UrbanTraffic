0fd7cf25d2807da20d542a6be49633644775aca6
zhiyingzhu1995 <zhiyingzhu1995@gmail.com>
2020-11-21 21:18:20 -0500

    Sliding scale scatterplot of numerical attribtues

---
+++ visualization/Slider_Scale_Scatterplots.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+from data.config import *
+from visualization.data_holder import AppData
+from visualization.graph_functions import *
+from visualization.data_filter import *
+
+df = AppData().taxi_trip_df
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+available_indicators = df.columns.values
+
+df = AppData().taxi_trip_df
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+available_indicators = df.columns.values
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],#available_indicators = all my numerical att
+                value='avg_trip_distance'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='avg_total_price'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df.index.year.min(),
+        max=df.index.year.max(),
+        value=df.index.year.max(),
+        marks={str(year): str(year) for year in df.index.year.unique()},
+        step=None
+    )
+])
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis-column', 'value'),
+    Input('yaxis-column', 'value'),
+    Input('xaxis-type', 'value'),
+    Input('yaxis-type', 'value'),
+    Input('year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    #dff = df.index.year == year_value
+    dff = df[df.index.year == year_value]
+
+    fig = px.scatter(x=dff[xaxis_column_name].values,
+                     y=dff[yaxis_column_name].values,
+                     hover_name=dff[yaxis_column_name].index)
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    fig.update_xaxes(title=xaxis_column_name,
+                     type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name,
+                     type='linear' if yaxis_type == 'Linear' else 'log')
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
