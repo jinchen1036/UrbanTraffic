6b9f1eb2a9e7b4151894bf9825d36c7dea09eb41
jinchen1036 <jinchen1036@users.noreply.github.com>
2020-11-24 13:36:02 -0500

    fix time selection

--- visualization/data_filter.py
+++ visualization/data_filter.py
@@ -25,11 +25,14 @@ def filter_by_time(trip_df,taxi_zone_df, year_range, month_range, days_range, ho
     if month_range[1] == 4 and days_range[1] == 31:
         days_range[1] = 30
 
-    start_time = '%04d-%02d-%02d %02d:00:00' % (year_range[0],month_range[0], days_range[0], hour_range[0])
-    end_time = '%04d-%02d-%02d %02d:00:00' % (year_range[1], month_range[1], days_range[1], hour_range[1])
+    start_day = '%04d-%02d-%02d' % (year_range[0],month_range[0], days_range[0])
+    end_day = '%04d-%02d-%02d' % (year_range[1], month_range[1], days_range[1])
+
+    start_time = '%02d:00:00' % hour_range[0]
+    end_time = '%02d:00:00'% hour_range[1]
 
     # filter by time
-    df = trip_df.loc[start_time:end_time]
+    df = trip_df.loc[start_day:end_day].between_time(start_time, end_time)
     df = df[np.isin(np.array(df.index.weekday,dtype=np.int), weekday_range)]
     df.reset_index(inplace=True)
     # group by zone
